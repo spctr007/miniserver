089f37d2ea4617d6ecc24b84b656991a0e96850f
Geoff Eslava <geoffrey.eslava@gmail.com>
2018-03-15 14:44:15 +0800

    Reduced centering percentage to 84%.

--- convert_srt_to_vtt.py
+++ convert_srt_to_vtt.py
@@ -17,7 +17,7 @@ def start_conversion(srt_path, vtt_path):
                 match = re.match(r'[\d]{2}:[\d]{2}:[\d]{2},[\d]{3}', line)
                 if match:
                     new_line_for_vtt = line.replace(',', '.').replace('\n', ' ').rstrip()
-                    full = new_line_for_vtt.join(' align:middle line:84%\n')
+                    full = new_line_for_vtt + ' align:middle line:84%\n'
                     vtt_file.write(full)
                 else:
                     vtt_file.write(line)
